bd86d735f0b56bd158b4d70c24acb010b0ce12a4
yoshitaka_takeuchi <6265686+yosatak@users.noreply.github.com>
2019-11-24 22:21:46 +0900

    comlpete include-for-loader

--- weblog.sh
+++ weblog.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
-readonly ROOT_DIR=/htdocs/weblog
+
+if [ -v $DOCUMENT_URL ];then
+    readonly ROOT_DIR=$(pwd)
+else
+    readonly ROOT_DIR=/htdocs/weblog
+fi
+#
 readonly LIB_DIR=${ROOT_DIR}/lib
 readonly SYS_DIR=${ROOT_DIR}/sys
 readonly TEMPLATE_DIR=${ROOT_DIR}/template
 readonly COMPONENT_DIR=${ROOT_DIR}/component
 readonly PATH=${SYS_DIR}:${LIB_DIR}:/bin:/usr/bin
 readonly MAIN_TEMPLATE=${TEMPLATE_DIR}/main.html
-
+readonly CGI_ROOT="/weblog.sh/"
 . lib-loader
-. router
+#. router
 . render
 
-router
+# router
 
 load_library template
+load_library virtual-associative-array
 PARAMS='aaa=aaa'
 template_loader article.html | include_loader | content_loader ${PARAMS} | render OK HTML
 
-
+echo $DOCUMENT_URI
