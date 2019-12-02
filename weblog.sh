40fc8b18937128be1cfee5d427cb03e8e5b4946b
yoshitaka_takeuchi <6265686+yosatak@users.noreply.github.com>
2019-12-02 12:37:39 +0900

    fix weblog.sh

--- weblog.sh
+++ weblog.sh
@@ -10,18 +10,16 @@ readonly LIB_DIR=${ROOT_DIR}/lib
 readonly SYS_DIR=${ROOT_DIR}/sys
 readonly TEMPLATE_DIR=${ROOT_DIR}/template
 readonly COMPONENT_DIR=${ROOT_DIR}/component
+readonly CONTENT_DIR=${ROOT_DIR}/content
+readonly ARTICLE_CONTENT_DIR=${CONTENT_DIR}/article
 readonly PATH=${SYS_DIR}:${LIB_DIR}:/bin:/usr/bin
 readonly MAIN_TEMPLATE=${TEMPLATE_DIR}/main.html
-readonly CGI_ROOT="/weblog.sh/"
+readonly CGI_ROOT="weblog.sh"
+
 . lib-loader
-#. router
+. router
 . render
-
-# router
-
 load_library template
-load_library virtual-associative-array
-PARAMS='aaa=aaa'
-template_loader article.html | include_loader | content_loader ${PARAMS} | render OK HTML
 
-echo $DOCUMENT_URI
+router
+
