f9981add315954c4965963f10a93f11b8068b161
yoshitaka_takeuchi <6265686+yosatak@users.noreply.github.com>
2019-12-04 22:09:29 +0900

    add tag page

--- weblog.sh
+++ weblog.sh
@@ -13,6 +13,7 @@ readonly TEMPLATE_DIR=${ROOT_DIR}/template
 readonly COMPONENT_DIR=${ROOT_DIR}/component
 readonly CONTENT_DIR=${ROOT_DIR}/content
 readonly ARTICLE_CONTENT_DIR=${CONTENT_DIR}/article
+readonly TAG_CONTENT_DIR=${CONTENT_DIR}/tag
 readonly PATH=${SYS_DIR}:${LIB_DIR}:/bin:/usr/bin
 readonly MAIN_TEMPLATE=${TEMPLATE_DIR}/main.html
 readonly CGI_ROOT="weblog.sh"
